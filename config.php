128efbc29241e46da6a82249395b2d01faccfc13
Meringue <44075123+PurpleLiquid@users.noreply.github.com>
2021-10-21 19:01:18 -0600

    re-adding
    sorry...

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+// $DB_HOST = 'vconroy.cs.uleth.ca';
+$DB_HOST = 'localhost';
+$DB_USER = 'user';
+$DB_PASS = 'password';
+$DB_NAME = 'library';
+
+$APP_NAME = 'Library Management Application';
+$APP_LOGO = '';
+$APP_TIMEZONE = 'America/Edmonton';
+
+$conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+
+
+?>
